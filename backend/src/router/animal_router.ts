c2088ba52d19ec5ea6d4f446b83801017181b2d0
Yacine <yacine.djabali@univ-alger.dz>
2026-08-20 19:59:29 +0100

    fix endpoint router

--- backend/src/router/animal_router.ts
+++ backend/src/router/animal_router.ts
@@ -2,6 +2,6 @@ import { Router } from "express";
 import { addAnimal , GetallAnimals} from "../controllers/animals.js";
 const router = Router();
 
-router.post("/animal" , addAnimal);
-router.post("/animal", GetallAnimals);
+router.post("/animal", addAnimal);
+router.get("/animal", GetallAnimals);
 export default router ;
